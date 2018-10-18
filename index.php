b3b9b45274cd7deb92775bef8bfb951deecd525e
Christina <christina.smithers@gmail.com>
2018-10-18 15:08:04 -0700

    Further UI fixes such as toggling open and close

--- index.php
+++ index.php
@@ -25,17 +25,26 @@ define("SCRIPT_ROOT", "http://localhost:8000/tools/");
   <link rel="stylesheet" href="./styles/style.css">
 
   <!-- scripts -->
+  <!-- Minified version of `es6-promise-auto` -->
+<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script> 
+<!-- styling from local-->
   <script src="https://tapstone.com/tools/js/jquery-ui.js"></script>
   <script src="https://tapstone.com/tools/js/foundation-datepicker.min.js"></script>
+  <!-- moar styling and ui stuff -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.1.0/lodash.min.js"></script>
   <script src="https://tapstone.com/tools/js/vendor/foundation.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
   <script src='https://cdn.jsdelivr.net/g/lodash@4(lodash.min.js+lodash.fp.min.js)'></script>
+  <!-- d3 crap to draw and export-->
   <script src="https://d3js.org/d3.v5.js"></script>  
   <script src="https://d3js.org/d3-collection.v1.min.js"></script>
   <script src="https://unpkg.com/xlsx/dist/xlsx.full.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script>
+  <!-- <script src="../node_modules/tableexport/src/stable/js/tableexport.min.js"></script> -->
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script> -->
+  <!-- <script type="text/javascript" src="tableExport.min.js"></script> -->
   <script src="https://d3js.org/d3-time.v1.min.js"></script>
 <script src="https://d3js.org/d3-time-format.v2.min.js"></script>
   <script src="./scripts/main.js"></script>
