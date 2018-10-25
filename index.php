768d877f02ad4b96fa7a07a71b6bc30ed04b719a
Christina <christina.smithers@gmail.com>
2018-10-25 16:37:40 -0700

    table now sorts recurively and filters

--- index.php
+++ index.php
@@ -14,45 +14,49 @@ define("SCRIPT_ROOT", "http://localhost:8000/tools/");
   <title>HONeY</title>
 
   <!-- jquery -->
-  <script src="https://tapstone.com/tools/js/vendor/jquery.js"></script>
+  <!-- <script src="https://tapstone.com/tools/js/vendor/jquery.js"></script> -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
   <!-- styles -->
-  <link rel="stylesheet" href="https://tapstone.com/tools/css/jquery-ui.css">
-  <link rel="stylesheet" href="https://tapstone.com/tools/css/foundation.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css">
   <link rel="stylesheet" href="https://tapstone.com/tools/foundation-icons/foundation-icons.css" />
-  <link rel="stylesheet" type="text/css" href="https://tapstone.com/tools/css/foundation-datepicker.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation-datepicker/1.5.6/css/foundation-datepicker.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.0/css/tableexport.min.css">
+  <link rel="stylesheet" href="https://mottie.github.io/tablesorter/addons/pager/jquery.tablesorter.pager.css">
+  <link rel="stylesheet" href="https://mottie.github.io/tablesorter/css/theme.default.css">
+  <link rel="stylesheet" href="https://mottie.github.io/tablesorter/addons/pager/jquery.tablesorter.pager.css">
   <link rel="stylesheet" href="./styles/style.css">
 
   <!-- scripts -->
   <!-- Minified version of `es6-promise-auto` -->
-<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script> 
-<!-- styling from local-->
-  <script src="https://tapstone.com/tools/js/jquery-ui.js"></script>
-  <script src="https://tapstone.com/tools/js/foundation-datepicker.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+  <!-- styling from local-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation-datepicker/1.5.6/js/foundation-datepicker.min.js"></script>
   <!-- moar styling and ui stuff -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.1.0/lodash.min.js"></script>
-  <script src="https://tapstone.com/tools/js/vendor/foundation.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
   <script src='https://cdn.jsdelivr.net/g/lodash@4(lodash.min.js+lodash.fp.min.js)'></script>
   <!-- d3 crap to draw and export-->
-  <script src="https://d3js.org/d3.v5.js"></script>  
+  <script src="https://d3js.org/d3.v5.js"></script>
   <script src="https://d3js.org/d3-collection.v1.min.js"></script>
   <script src="https://unpkg.com/xlsx/dist/xlsx.full.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script>
-  <!-- <script src="../node_modules/tableexport/src/stable/js/tableexport.min.js"></script> -->
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script> -->
-  <!-- <script type="text/javascript" src="tableExport.min.js"></script> -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.2/js/tableexport.min.js"></script>
   <script src="https://d3js.org/d3-time.v1.min.js"></script>
-<script src="https://d3js.org/d3-time-format.v2.min.js"></script>
+  <script src="https://d3js.org/d3-time-format.v2.min.js"></script>
+
+  <!-- load jQuery and tablesorter scripts to deal with the sorting and filtering-->
+  <script src="https://cdn.jsdelivr.net/npm/tablesorter@2.31.0/dist/js/jquery.tablesorter.combined.min.js"></script>
+  <!-- tablesorter widgets (optional) -->
+  <script src="https://mottie.github.io/tablesorter/js/widgets/widget-sortTbodies.js"></script>
+  <script src="https://mottie.github.io/tablesorter/addons/pager/jquery.tablesorter.pager.js"></script>
+  <!-- and of course the main one -->
   <script src="./scripts/main.js"></script>
-
-
-  <!-- Busy Load JS -->
-  <script src="https://cdn.jsdelivr.net/npm/busy-load/dist/app.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/busy-load/dist/app.min.css" rel="stylesheet">
 
 
 </head>
@@ -76,53 +80,53 @@ define("SCRIPT_ROOT", "http://localhost:8000/tools/");
   <br />
 
   <div class="row expanded">
-    <div class="row" </div> <div class="large-12 columns">
-      <div class="callout">
-        <h3> HONeY Tool</h3>
-        <p>Make reports for a chosen range of dates.</p>
-        <p style="display: inline-block; margin: 5px;">Data ready from Netsphere?
-          <div style="display: inline-block; margin: 5px;" id="netsphereStatus"></div>
-        </p>
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="callout">
+          <h3> HONeY Tool</h3>
+          <p>Make reports for a chosen range of dates.</p>
+          <p style="display: inline-block; margin: 5px;">Data ready from Netsphere?
+            <div style="display: inline-block; margin: 5px;" id="netsphereStatus"></div>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="large-12 columns">
-      <div class="callout" id="error-box">
-        <div id="errorContainer"></div>
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="callout" id="error-box">
+          <div id="errorContainer"></div>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="row">
-    <div class="large-12 columns" id="date-pickers">
-      <form>
-        <div class="small-4 large-2 columns" id="start-date-box">
-          <label for="start-date" class="text-right left">Start Date</label>
-          <input type="text" id="start-date">
-        </div>
-        <div class="small-4 large-2 columns" id="end-date-box">
-          <label for="end-date" class="text-right left">End Date</label>
-          <input type="text" id="end-date">
-        </div>
-        <div class="small-4 large-4 columns"></div>
-      </form>
+    <div class="row">
+      <div class="large-12 columns" id="date-pickers">
+        <form>
+          <div class="small-4 large-2 columns" id="start-date-box">
+            <label for="start-date" class="text-right left">Start Date</label>
+            <input type="text" id="start-date">
+          </div>
+          <div class="small-4 large-2 columns" id="end-date-box">
+            <label for="end-date" class="text-right left">End Date</label>
+            <input type="text" id="end-date">
+          </div>
+          <div class="small-4 large-4 columns"></div>
+        </form>
+      </div>
     </div>
-  </div>
 
-  <div class="row" style="width: 100%">
-    <div class="large-12 columns">
-              <!--		Show Numbers Of Rows 		-->
-  <!-- <select class="form-control" name="state" id="maxRows">
-    <option value="5000">Show ALL Rows ▼</option>
-    <option value="25">25</option>
-    <option value="50">50</option>
-    <option value="100">100</option>
-  </select><br/> -->
-      <table id="table">
-      </table>
+    <div class="row" style="width: 100%;">
+      <!-- pager -->
+   
+      <div class="large-12 columns">
+
+        <table id="table" class="tablesorter">
+          <div class="loader">
+            <div class="spinner"></div>
+          </div>
+        </table>
+      </div>
     </div>
-  </div>
   </div>
 
   </div>
